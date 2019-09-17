6ca6e3ebb32db82f1f14f1e1484969c291194a81
Ralf Schlatterbeck <rsc@runtux.com>
2019-09-17 18:13:54 +0200

    Update test and templates for latest schema change

--- test/search_tt.py
+++ test/search_tt.py
@@ -45,6 +45,9 @@ properties = \
         , ( 'duration'
           , ['admin', 'hr']
           )
+        , ( 'durations_allowed'
+          , ['admin', 'hr']
+          )
         , ( 'is_valid'
           , ['admin', 'hr']
           )
@@ -55,6 +58,12 @@ properties = \
           , ['admin', 'hr']
           )
         , ( 'time_project'
+          , ['admin', 'hr']
+          )
+        , ( 'time_wp_summary_no'
+          , ['admin', 'hr']
+          )
+        , ( 'travel'
           , ['admin', 'hr']
           )
         ]
